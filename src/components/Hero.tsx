a564e67bc53935ee837192eeb96dd78d4a9cfbb3
Sahil-Hode <sahilhode67@gmail.com>
2026-09-19 19:59:42 +0530

    feat: add Hero component with GlowRing and BrandWord

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import { BrandWord } from "./BrandWord";
 import { content } from "./content";
-import { CTAButton } from "./CTAButton";
 import { GlowRing } from "./GlowRing";
 
 export function Hero() {
@@ -13,14 +12,6 @@ export function Hero() {
         word={content.brand.wordmark}
         label={content.brand.name}
       />
-
-      <div className="lp-bottom">
-        <p className="lp-tagline">{content.hero.tagline}</p>
-        <CTAButton href={content.cta.href} size="lg" className="lp-bottom__cta">
-          {content.cta.label}
-        </CTAButton>
-        <p className="lp-guarantee">{content.hero.guarantee}</p>
-      </div>
 
       <div className="lp-grain" aria-hidden="true" />
     </section>
